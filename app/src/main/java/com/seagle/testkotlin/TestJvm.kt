54d8bd2e97a74f6c21683c19e25f83c47f60dc53
yidao <49662990@qq.com>
2022-08-17 22:34:24 +0800

    test

--- app/src/main/java/com/seagle/testkotlin/TestJvm.kt
+++ app/src/main/java/com/seagle/testkotlin/TestJvm.kt
@@ -5,10 +5,14 @@ package com.seagle.testkotlin
  */
 class TestJvm {
 
-    var data :Int=0
+    var data: Int = 0
 
-    companion object{
+    var isEnable: Boolean = false
+
+    lateinit var ID:String
+
+    companion object {
         @JvmStatic
-        val INSTANCE:TestJvm = TestJvm();
+        val INSTANCE: TestJvm = TestJvm();
     }
 }
